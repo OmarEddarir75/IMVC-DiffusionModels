09c988378f36e825b74dc727943a89194df24af0
EddarirOmar <eddarir.omar@gmail.com>
2026-04-05 13:52:36 +0100

    UP-TO-DATE

--- IMVCDC/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-IMVCDC: Input-space Multiview Diffusion Completion
-
-A diffusion-based approach for incomplete multi-view clustering using
-input-space (pixel-level) diffusion for missing-view generation.
-"""
-
-__version__ = "0.1.0"
